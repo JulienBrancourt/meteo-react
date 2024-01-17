88801a607c3bd69d0bc80cf125a91081d73750e6
ElyanisJ <brancourt.julien@hotmail.fr>
2024-01-17 15:35:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import loader from "./assets/loader.svg";
+import "./App.css"
+
+function App() {
+	return (
+		<main>
+			<div className="loader-container">
+				<img src={loader} alt="loading icon" />
+			</div>
+			<p className="city-name">Amiens</p>
+			<p className="country-name">France</p>
+			<p className="temperature">17°</p>
+			<div className="info-icon-container">
+				<img src="/icons/01d.svg" className="info-icon" alt="" />
+			</div>
+		</main>
+	);
+}
+
+export default App;
