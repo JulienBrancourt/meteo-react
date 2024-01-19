67585e015ae6e53f60af3c5e45996780a8ddbb81
ElyanisJ <brancourt.julien@hotmail.fr>
2024-01-19 16:12:22 +0100

    ajout appel API et hydratation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
+import { useEffect, useState } from "react";
 import loader from "./assets/loader.svg";
-import "./App.css"
+import "./App.css";
+const APIKEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 function App() {
+	const [meteo, setMeteo] = useState(null);
+
+	useEffect(() => {
+		fetch(`http://api.airvisual.com/v2/nearest_city?key=${APIKEY}`)
+			.then((response) => {
+				console.log(response);
+				return response.json();
+			})
+			.then((dataAPI) => {
+				console.log(dataAPI);
+				setMeteo({
+					ville: dataAPI.data.city,
+					pays: dataAPI.data.country,
+					icon: dataAPI.data.current.weather.ic,
+					temperature: dataAPI.data.current.weather.tp,
+				});
+			});
+	}, []);
 	return (
 		<main>
-			<div className="loader-container">
+			{/* on affiche le loader en lui donnant la classe active si meteo est faux */}
+			<div className={`loader-container ${!meteo && "active"}`}>
 				<img src={loader} alt="loading icon" />
 			</div>
-			<p className="city-name">Amiens</p>
-			<p className="country-name">France</p>
-			<p className="temperature">17°</p>
-			<div className="info-icon-container">
-				<img src="/icons/01d.svg" className="info-icon" alt="" />
-			</div>
+
+			{meteo && (
+				<>
+					<p className="city-name">{meteo.ville}</p>
+					<p className="country-name">{meteo.pays}</p>
+					<p className="temperature">{meteo.temperature}°</p>
+					<div className="info-icon-container">
+						<img src={`/icons/${meteo.icon}.svg`} className="info-icon" alt="" />
+					</div>
+				</>
+			)}
 		</main>
 	);
 }
