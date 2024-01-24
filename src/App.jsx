a7398a012d6b31f5a4acdfd9810e8e0d393071a5
ElyanisJ <brancourt.julien@hotmail.fr>
2024-01-24 15:52:22 +0100

    gestion des erreurs

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react";
 import loader from "./assets/loader.svg";
+import Browser from "./assets/browser.svg";
 import "./App.css";
 const APIKEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 function App() {
 	const [meteo, setMeteo] = useState(null);
+	const [errorInfo, setErrorInfo] = useState(null);
 
 	useEffect(() => {
 		fetch(`http://api.airvisual.com/v2/nearest_city?key=${APIKEY}`)
 			.then((response) => {
 				console.log(response);
-				return response.json();
+				// les erreur 400 à 599 ne sont pas naturellement prises en compte par le catch, donc si le ok est faux on crée une nouvelle erreur  ("throw new Error" force l'exécution du catch)
+				if (!response.ok) throw new Error(`Error ${response.status}, ${response.statusText}`)
+
+				return response.json
 			})
 			.then((dataAPI) => {
 				console.log(dataAPI);
@@ -20,12 +25,16 @@ function App() {
 					icone: dataAPI.data.current.weather.ic,
 					temperature: dataAPI.data.current.weather.tp,
 				});
+			})
+			.catch((err) => {
+				console.error(err.message);
+				setErrorInfo(err.message);
 			});
 	}, []);
 	return (
 		<main>
 			{/* on affiche le loader en lui donnant la classe active si meteo est faux */}
-			<div className={`loader-container ${!meteo && "active"}`}>
+			<div className={`loader-container ${!meteo && !errorInfo && "active"}`}>
 				<img src={loader} alt="loading icon" />
 			</div>
 
@@ -35,8 +44,19 @@ function App() {
 					<p className="country-name">{meteo.pays}</p>
 					<p className="temperature">{meteo.temperature}°</p>
 					<div className="info-icon-container">
-						<img src={`/icons/${meteo.icone}.svg`} className="info-icon" alt="" />
+						<img
+							src={`/icons/${meteo.icone}.svg`}
+							className="info-icon"
+							alt=""
+						/>
 					</div>
+				</>
+			)}
+
+			{errorInfo && !meteo && (
+				<>
+					<p className="error-information">{errorInfo}</p>
+					<img src={Browser} alt="error icon" />
 				</>
 			)}
 		</main>
